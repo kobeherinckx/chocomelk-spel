c3dadbc92cce0a8d4fd77a05cded1ac5f1efbddb
kobeherinckx <136462363+kobeherinckx@users.noreply.github.com>
2023-06-13 14:10:00 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    let ùknelnfnfe: game.LedSprite = null
+    ùknelnfnfe.ifOnEdgeBounce()
+})
+let sprite: game.LedSprite = null
+sprite.move(1)
+basic.forever(function () {
+    music.playMelody("C5 B A G F E D C ", 500)
+})
